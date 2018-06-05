1f013c2d6110f3d9b5590e3ce253af005ea6a8c6
Jakub Pasula <kuba10pl@gmail.com>
2018-06-05 12:49:07 +0200

    Add files via upload

---
+++ gen_obraz.m
@@ -0,0 +1,18 @@
+%generator obrazu generuje obraz pol bialy pol czarny oraz czarny kwadrat na bialym tle
+obraz=zeros(256,256);
+obraz=uint8(obraz);
+for i=1:256
+    for j=1:128
+        obraz(i,j)=255;
+    end
+end       
+imwrite(obraz,'pol_na_pol.bmp');
+
+obraz=ones(256,256);
+obraz=obraz*255;
+for i=128-60:128+60
+    for j=128-60:128+60
+        obraz(i,j)=0;
+    end
+end 
+imwrite(obraz,'kwadrat.bmp');
